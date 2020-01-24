ab9a53a7fbb938447e2fad6cf08b2f0754ff5258
brockelmore <31553173+brockelmore@users.noreply.github.com>
2020-01-23 19:26:40 -0500

    a

--- ethabi/src/param.rs
+++ ethabi/src/param.rs
@@ -84,7 +84,7 @@ impl<'a> Visitor<'a> for ParamVisitor {
 					ParamType::Tuple(_) => {
 						let tuple_params = components.ok_or_else(|| Error::missing_field("components"))?;
 						Ok(ParamType::Array(Box::new(ParamType::Tuple(
-							tuple_params.into_iter().map(|param| {println!("array tuple: {:?}", param.kind); param.kind}).map(Box::new).collect(),
+							tuple_params.into_iter().map(|param| {println!("name: {:?}; array tuple: {:?}", name, param.kind); param.kind}).map(Box::new).collect(),
 						))))
 					}
 					_ => Ok(ParamType::Array(inner_param_type)),
@@ -94,7 +94,7 @@ impl<'a> Visitor<'a> for ParamVisitor {
 						let tuple_params = components.ok_or_else(|| Error::missing_field("components"))?;
 						Ok(ParamType::FixedArray(
 							Box::new(ParamType::Tuple(
-								tuple_params.into_iter().map(|param| {println!("fixed array tuple: {:?}", param.kind); param.kind}).map(Box::new).collect(),
+								tuple_params.into_iter().map(|param| {println!("name: {:?}; fixed array tuple: {:?}", name, param.kind); param.kind}).map(Box::new).collect(),
 							)),
 							size,
 						))
